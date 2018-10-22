4e506c413a81c11a60288e66b56c50a1e8dd4c8f
owen francis <owenfrancis142@gmail.com>
2018-10-23 00:03:05 +0100

    Added base GUI class and comments to explain objectives

---
+++ src/view/GUICreation.java
@@ -0,0 +1,33 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.event.*;
+import java.io.File; 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import character.*;
+
+public class GUICreation {
+
+	/*This class will construct the GUI, which will include a button to generate a character, 
+	which will save the character info to a text file.
+	There will also be two more buttons, for loading the first and second character, which will then display their stats.
+	And one final button that will run a fight between the two characters, and save the log of every attack to a file,
+	which will then be loaded and displayed, ending with a victory message
+	*/
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GUICreation g = new GUICreation();
+	}
+	
+	public GUICreation() {
+		
+	}
+
+}
